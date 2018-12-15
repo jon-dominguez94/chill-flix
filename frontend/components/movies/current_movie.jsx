b3a7bd5bfee2f30caf396cf29da1b92b04f364a5
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-15 13:23:29 -0800

    Setup html for current video player

--- frontend/components/movies/current_movie.jsx
+++ frontend/components/movies/current_movie.jsx
@@ -18,14 +18,40 @@ class CurrentMovie extends React.Component {
       return (<div></div>);
     }
     return(
-
-      <video className="current-video" width="100vw" height="auto" controls>
-        {/* <source src="/assets/com.mp4" type="video/mp4" /> */}
-        <source src={window.oldvid} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <div id="video-container">
+        <video className="current-video" width="100vw" height="auto" controls>
+          {/* <source src="/assets/com.mp4" type="video/mp4" /> */}
+          <source src={window.oldvid} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div id="video-controls">
+          <button type="button" id="play-pause" value="0">Play</button>
+          <input type="range" id="seek-bar" defaultValue="0"/>
+          <button type="button" id="mute">Mute</button>
+          <input type="range" id="volume-bar" min="0" max="1" step="0.1" defaultValue="1" />
+          <button type="button" id="full-screen">Full-Screen</button>
+        </div>
+      </div>
 
     )
+
+    // return (
+    //   <div id="video-container" width="100vw" height="auto" controls>
+    //     {/* <!-- Video --> */}
+    //     <video id="video" width="100vw" height="auto">
+    //       <source src={window.oldvid} type="video/mp4" />
+    //       <p>Your browser doesn't support HTML5 video.</p>
+    //     </video>
+    //     {/* <!-- Video Controls --> */}
+    //     {/* <div id="video-controls">
+    //       <button type="button" id="play-pause">Play</button>
+    //       <input type="range" id="seek-bar" value="0"/>
+    //       <button type="button" id="mute">Mute</button>
+    //       <input type="range" id="volume-bar" min="0" max="1" step="0.1" value="1"/>
+    //       <button type="button" id="full-screen">Full-Screen</button>
+    //     </div> */}
+    //   </div>
+    // );
   }
 };
 
