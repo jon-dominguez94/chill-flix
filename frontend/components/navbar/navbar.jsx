58f66363a224062f9e96574e47bace870aab6ce1
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-18 16:14:07 -0800

    Get search animation working

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,97 +1,112 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = (props) => {
-  if (props.currentUser === undefined) {
-    return (
-      <div className="splash-container">
-
-        <div className="main-header">
-          <Link to="/">
-            <img className="main-logo" src="https://fontmeme.com/permalink/181212/c5c4b3134061f86d06de9895b1ea5522.png" border="0" />
-          </Link>
-          <div className="session-btn signin" >
-            <Link className="session-btn signin" to='/login'>Sign In</Link>
-          </div>
-        </div>
-
-        <div className="signup-container">
-          <div className="signup-msg">
-            No pesky contracts
-          </div>
-          <div className="signup-sub-msg">
-            Join today, cancel anytime.
-          </div>
-          <div className="session-btn signup" >
-            <Link className="session-btn signup" to='/signup'>S I G N U P</Link>
-          </div>
-        </div>
+class Navbar extends React.Component {
 
 
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <div className="main-header">
-          <div>
+  constructor(props){
+    super(props);
+  }
+
+  expand(){
+    // document.getElementById("search-input").style.display = "inline-block";
+    // document.getElementById("search-controls").style.borderColor = "white";
+    document.getElementById("search-input").classList.toggle('not-displayed');
+    document.getElementById("search-controls").classList.toggle('white');
+  }
+
+  render(){
+    if (this.props.currentUser === undefined) {
+      return (
+        <div className="splash-container">
+
+          <div className="main-header">
             <Link to="/">
               <img className="main-logo" src="https://fontmeme.com/permalink/181212/c5c4b3134061f86d06de9895b1ea5522.png" border="0" />
             </Link>
-          </div>
-          <div className="nav-items">
-            <div className="nav-links">
-              <Link className="nav-link-item" to="/">Home</Link>
-              <Link className="nav-link-item" to="/list">My List</Link>
-              {/* <h1 className="nav-link-item">Welcome, {props.currentUser.email}!</h1> */}
+            <div className="session-btn signin" >
+              <Link className="session-btn signin" to='/login'>Sign In</Link>
             </div>
-            <div className="nav-controls">
-                <div className="nav-items wsearch">
+          </div>
 
-                  <div className="search-controls">
-                    <div className="search-btn" >
-                      <i className="fa fa-search"></i>
+          <div className="signup-container">
+            <div className="signup-msg">
+              No pesky contracts
+            </div>
+            <div className="signup-sub-msg">
+              Join today, cancel anytime.
+            </div>
+            <div className="session-btn signup" >
+              <Link className="session-btn signup" to='/signup'>S I G N U P</Link>
+            </div>
+          </div>
+
+
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div className="main-header">
+            <div>
+              <Link to="/">
+                <img className="main-logo" src="https://fontmeme.com/permalink/181212/c5c4b3134061f86d06de9895b1ea5522.png" border="0" />
+              </Link>
+            </div>
+            <div className="nav-items">
+              <div className="nav-links">
+                <Link className="nav-link-item" to="/">Home</Link>
+                <Link className="nav-link-item" to="/list">My List</Link>
+                {/* <h1 className="nav-link-item">Welcome, {this.props.currentUser.email}!</h1> */}
+              </div>
+              <div className="nav-controls">
+                  <div className="nav-items wsearch">
+
+                  <div id="search-controls" className="search-controls">
+                      <div className="search-btn" onClick={() => this.expand()} >
+                        <i className="fa fa-search"></i>
+                      </div>
+                      <input id="search-input" type="text" placeholder="Search by title"/>
                     </div>
-                    <input type="text" placeholder="Search by title"/>
-                  </div>
 
 
-                  <div className="profile-dropdown" >
-                  {/* <div className="profile-dropdown"> */}
-                    <img className="profile-logo" src="https://assets.nflxext.com/ffe/profiles/avatars_v2/32x32/PICON_025.png" alt=""/>
-                    <i className="fa fa-caret-down"></i>
-                  </div>
-                  <div id="my-dropdown" className="dropdown-content" onMouseOver={() => {
-                    const openDropdown = document.getElementById("my-dropdown");
-                    if (!openDropdown.classList.contains('show')) {
-                      openDropdown.classList.add('show');
-                    }
+                    <div className="profile-dropdown" >
+                    {/* <div className="profile-dropdown"> */}
+                      <img className="profile-logo" src="https://assets.nflxext.com/ffe/profiles/avatars_v2/32x32/PICON_025.png" alt=""/>
+                      <i className="fa fa-caret-down"></i>
+                    </div>
+                    <div id="my-dropdown" className="dropdown-content" onMouseOver={() => {
+                      const openDropdown = document.getElementById("my-dropdown");
+                      if (!openDropdown.classList.contains('show')) {
+                        openDropdown.classList.add('show');
+                      }
+                    }} onMouseOut={() => {
+                      const openDropdown = document.getElementById("my-dropdown");
+                      if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                      }
+                    }}>
+                        <section className="drop-item manage-link" >Manage Profiles</section>
+                        <section className="drop-item" onClick={() => this.props.logout()}>Logout of Chillflix</section>
+                      </div>
+                    </div>
+                    <div className="nav-control-wrapper" onMouseOver={() => {
+                      const openDropdown = document.getElementById("my-dropdown");
+                      if (!openDropdown.classList.contains('show')) {
+                        openDropdown.classList.add('show');
+                      }
                   }} onMouseOut={() => {
-                    const openDropdown = document.getElementById("my-dropdown");
-                    if (openDropdown.classList.contains('show')) {
-                      openDropdown.classList.remove('show');
-                    }
-                  }}>
-                      <section className="drop-item manage-link" >Manage Profiles</section>
-                      <section className="drop-item" onClick={() => props.logout()}>Logout of Chillflix</section>
-                    </div>
-                  </div>
-                  <div className="nav-control-wrapper" onMouseOver={() => {
-                    const openDropdown = document.getElementById("my-dropdown");
-                    if (!openDropdown.classList.contains('show')) {
-                      openDropdown.classList.add('show');
-                    }
-                }} onMouseOut={() => {
-                 const openDropdown = document.getElementById("my-dropdown");
-                if (openDropdown.classList.contains('show')) {
-                  openDropdown.classList.remove('show');
-                }
-              }}></div>
+                  const openDropdown = document.getElementById("my-dropdown");
+                  if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                  }
+                }}></div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 };
 
