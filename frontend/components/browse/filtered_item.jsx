ce252cbe0762f276eaf7593785a74f0cd9683e8f
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-19 11:09:01 -0800

    Style filtered items

--- frontend/components/browse/filtered_item.jsx
+++ frontend/components/browse/filtered_item.jsx
@@ -10,6 +10,17 @@ const FilteredItem = (props) => {
       {/* <img className="movie-tn" src={props.movie.thumbnail} /> */}
       <img className="movie-tn" src={window.oldtn} />
 
+
+      <div className="search-overlay">
+
+        <div className="search-play">
+          <Link to={`/watch/${props.movie.id}`}>
+            <i className="fa fa-play-circle-o"></i>
+          </Link>
+        </div>
+
+      </div>
+
       <p className="filter-title">{props.movie.title}</p>
       {/* <div className="tn-info">
         <a href="#" className="round-button">
